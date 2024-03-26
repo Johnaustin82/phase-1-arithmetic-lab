4ed22f0235e74bf40bc5e2809d7ff9d62a4a5167
John Austin Ngode <austinngode23@gmail.com>
2024-03-26 12:16:41 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1=31
+let num2=2
+let multiply=num1*num2
+
+let random = Math.floor(Math.random()*Number.MAX_SAFE_INTEGER)+1
+
+let num3 = 9; 
+let num4 = 5; 
+let mod = (num3 % num4); 
+if (mod === 4) {
+    console.log("The remainder is 4.");
+} else {
+    console.log("The remainder is not equal to 4.");
+}
+
+let set = [3, 8, 3, 20, 14]; 
+let max = Math.max(...set);
+console.log(max); 
